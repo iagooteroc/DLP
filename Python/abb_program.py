968a5958a51e0c9a161024517388449b0cfb27ce
lKoi <iago_neosky@hotmail.com>
2017-10-02 12:00:13 +0200

    python program

---
+++ Python/abb_program.py
@@ -0,0 +1,49 @@
+from abb_v3 import *
+
+def preorder(bst):
+	preorder_node(bst.root)
+	print("")
+
+def preorder_node(bst):
+	print(" ( ", end="")
+	if not isEmpty(bst.get()):
+		if not isEmpty(bst.get().leftChild.get()) or not isEmpty(bst.get().rightChild.get()):
+			print(bst.get().key, end="")
+			preorder_node(bst.get().leftChild)
+			preorder_node(bst.get().rightChild)
+		else:
+			print(bst.get().key, end="")
+	print(" ) ", end="")
+
+T = BinarySearchTree()
+insertKey(T, 4)
+insertKey(T, 4)
+insertKey(T, 2)
+insertKey(T, 6)
+insertKey(T, 1)
+insertKey(T, 3)
+insertKey(T, 5)
+insertKey(T, 7)
+
+preorder(T)
+
+print("Search 1...", str(root(searchKey(T, 1))))
+print("Search 2...", str(root(searchKey(T, 2))))
+print("Search 3...", str(root(searchKey(T, 3))))
+print("Search 4...", str(root(searchKey(T, 4))))
+print("Search 5...", str(root(searchKey(T, 5))))
+print("Search 6...", str(root(searchKey(T, 6))))
+print("Search 7...", str(root(searchKey(T, 7))))
+
+print("Remove 5...", end="")
+removeKey(T, 5)
+preorder(T)
+print("Remove 6...", end="")
+removeKey(T, 6)
+preorder(T)
+print("Remove 4...", end="")
+removeKey(T, 4)
+preorder(T)
+print("Remove 2...", end="")
+removeKey(T, 2)
+preorder(T)
--- Python/abb_v3.py
+++ Python/abb_v3.py
@@ -18,145 +18,44 @@ class BinarySearchTree:
 	def __init__(self, root = Pos(None)):
 		self.root = root
 
-	def insertKey(self, key):
-		if (self.root.get() is None):
-			self.root.set(Node(key))
+#----------------------------------------------------------------
+
+def insertKey(bst, key):
+	if (bst.root.get() is None):
+		bst.root.set(Node(key))
+	else:
+		insert_i(bst.root, key)
+
+def insert_r(currentNode, key):
+	if key < currentNode.get().key:
+		if not (isEmpty(currentNode.get().leftChild.get())):
+			insert_r(currentNode.get().leftChild, key)
 		else:
-			self.insert_r(self.root, key)
-
-	def insert_r(self, currentNode, key):
-		if key < currentNode.get().key:
-			if not (isEmpty(currentNode.get().leftChild.get())):
-				self.insert_r(currentNode.get().leftChild, key)
-			else:
-				currentNode.get().leftChild.set(Node(key))
-		elif key > currentNode.get().key:
-			if not (isEmpty(currentNode.get().rightChild.get())):
-				self.insert_r(currentNode.get().rightChild, key)
-			else:
-				currentNode.get().rightChild.set(Node(key))
-		# ignore duplicates
-
-	def insert_i(self, currentNode, key):
-		father = Pos(None)
-		child = currentNode
-		while (child.get() is not None) and (child.get().key != key):
-			father = child
-			if key < child.get().key:
-				child = child.get().leftChild
-			else:
-				child = child.get().rightChild
-		if (child.get() is None):
-			if (key < father.get().key):
-				father.get().leftChild.set(Node(key))
-			else:
-				father.get().rightChild.set(Node(key))
-		# ignore duplicates
-
-	def removeKey(self, key):
-		self.remove_i(self.root, key)
-
-	def remove_r(self, currentNode, key):
-		aux = Pos(None)
-		# procedure to be called when the node to be removed
-		# has two childs, to replace it with the greatest key
-		# from its left subtree.
-		def sup2(bst):
-			if (bst.get().rightChild.get() is not None):
-				sup2(bst.get().rightChild)
-			else:
-				aux.get().key = bst.get().key
-				bst.set(bst.get().leftChild.get())
-
-		# if its not in the tree, nothing is done
-		if (currentNode.get() is not None):
-			if (key < currentNode.get().key):
-				self.remove_r(currentNode.get().leftChild, key)
-			elif (key > currentNode.get().key):
-				self.remove_r(currentNode.get().rightChild, key)
-			else:
-				aux.set(currentNode.get())
-				if currentNode.get().leftChild.get() is None:
-					currentNode.set(currentNode.get().rightChild.get())
-				elif currentNode.get().rightChild.get() is None:
-					currentNode.set(currentNode.get().leftChild.get())
-				else:
-					sup2(currentNode.get().leftChild)
-
-	def remove_i(self, currentNode, key):
-		sup = currentNode	# node to be removed
-		fSup = Pos(None)	# father of the node to be removed
-		# search for the node to be removed
-		while (not isEmpty(sup.get())) and (sup.get().key != key):
-			fSup = sup
-			if (key < sup.get().key):
-				sup = sup.get().leftChild
-			else:
-				sup = sup.get().rightChild
-		# if its not in the tree, nothing is done
-		if (not isEmpty(sup.get())):
-			# count the number of childs
-			childNum = 0
-			if (not isEmpty(sup.get().leftChild.get())):
-				childNum += 1
-			if (not isEmpty(sup.get().rightChild.get())):
-				childNum += 1
-			# remove leaf node
-			if (childNum == 0):
-				if (isEmpty(fSup.get())):
-					# the root was the only node of the tree
-					currentNode.set(None)
-				elif (fSup.get().leftChild.get() == sup.get()):
-					fSup.get().leftChild.set(None)
-				else:
-					fSup.get().rightChild.set(None)
-			# remove node with only one child
-			elif (childNum == 1):
-				if (isEmpty(sup.get().leftChild.get())):
-					childNotEmpty = sup.get().rightChild
-				else:
-					childNotEmpty = sup.get().leftChild
-				if (isEmpty(fSup.get())):
-					# the root is removed
-					currentNode.set(childNotEmpty.get())
-				elif (fSup.get().leftChild.get() == sup.get()):
-					fSup.get().leftChild.set(childNotEmpty.get())
-				else:
-					fSup.get().rightChild.set(childNotEmpty.get())
-			# remove node with two childs
-			else:
-				fSup = sup
-				# supLeftMax: node with the greatest value from the
-				# left subtree
-				supLeftMax = sup.get().leftChild
-				while (not isEmpty(supLeftMax.get().rightChild.get())):
-					fSup = supLeftMax
-					supLeftMax = supLeftMax.get().rightChild
-				sup.get().key = supLeftMax.get().key
-				if (fSup.get() == sup.get()):
-					fSup.get().leftChild.set(supLeftMax.get().leftChild.get())
-				else:
-					fSup.get().rightChild.set(supLeftMax.get().leftChild.get())
-
-
-
-def isEmpty(bst):
-	return bst is None
-
-def preorder(bst):
-	preorder_node(bst.root)
-	print("")
-
-def preorder_node(bst):
-	print(" ( ", end="")
-	if not isEmpty(bst.get()):
-		if not isEmpty(bst.get().leftChild.get()) or not isEmpty(bst.get().rightChild.get()):
-			print(bst.get().key, end="")
-			preorder_node(bst.get().leftChild)
-			preorder_node(bst.get().rightChild)
+			currentNode.get().leftChild.set(Node(key))
+	elif key > currentNode.get().key:
+		if not (isEmpty(currentNode.get().rightChild.get())):
+			insert_r(currentNode.get().rightChild, key)
 		else:
-			print(bst.get().key, end="")
-	print(" ) ", end="")
+			currentNode.get().rightChild.set(Node(key))
+	# ignore duplicates
+
+def insert_i(currentNode, key):
+	father = Pos(None)
+	child = currentNode
+	while (child.get() is not None) and (child.get().key != key):
+		father = child
+		if key < child.get().key:
+			child = child.get().leftChild
+		else:
+			child = child.get().rightChild
+	if (child.get() is None):
+		if (key < father.get().key):
+			father.get().leftChild.set(Node(key))
+		else:
+			father.get().rightChild.set(Node(key))
+	# ignore duplicates
+
+#----------------------------------------------------------------
 
 def searchKey(bst, key):
 	return search_i(bst.root, key)
@@ -180,29 +79,109 @@ def search_i(currentNode, key):
 			node = node.get().rightChild
 	return BinarySearchTree(node)
 
+#----------------------------------------------------------------
 
-"""
+def leftChild(node):
+	return node.get().leftChild
+
+#----------------------------------------------------------------
+
+def rightChild(node):
+	return node.get().rightChild
+
+#----------------------------------------------------------------
+
+def root(node):
+	return node.root.get().key
+
+#----------------------------------------------------------------
+
+def isEmpty(bst):
+	return bst is None
+
+#----------------------------------------------------------------
+
+def remove_r(currentNode, key):
+	aux = Pos(None)
+	# procedure to be called when the node to be removed
+	# has two childs, to replace it with the greatest key
+	# from its left subtree.
+	def sup2(bst):
+		if (bst.get().rightChild.get() is not None):
+			sup2(bst.get().rightChild)
+		else:
+			aux.get().key = bst.get().key
+			bst.set(bst.get().leftChild.get())
+	# if its not in the tree, nothing is done
+	if (currentNode.get() is not None):
+		if (key < currentNode.get().key):
+			remove_r(currentNode.get().leftChild, key)
+		elif (key > currentNode.get().key):
+			remove_r(currentNode.get().rightChild, key)
+		else:
+			aux.set(currentNode.get())
+			if currentNode.get().leftChild.get() is None:
+				currentNode.set(currentNode.get().rightChild.get())
+			elif currentNode.get().rightChild.get() is None:
+				currentNode.set(currentNode.get().leftChild.get())
+			else:
+				sup2(currentNode.get().leftChild)
+
+def remove_i(currentNode, key):
+	sup = currentNode	# node to be removed
+	fSup = Pos(None)	# father of the node to be removed
+	# search for the node to be removed
+	while (not isEmpty(sup.get())) and (sup.get().key != key):
+		fSup = sup
+		if (key < sup.get().key):
+			sup = sup.get().leftChild
+		else:
+			sup = sup.get().rightChild
+	# if its not in the tree, nothing is done
+	if (not isEmpty(sup.get())):
+		# count the number of childs
+		childNum = 0
+		if (not isEmpty(sup.get().leftChild.get())):
+			childNum += 1
+		if (not isEmpty(sup.get().rightChild.get())):
+			childNum += 1
+		# remove leaf node
+		if (childNum == 0):
+			if (isEmpty(fSup.get())):
+				# the root was the only node of the tree
+				currentNode.set(None)
+			elif (fSup.get().leftChild.get() == sup.get()):
+				fSup.get().leftChild.set(None)
+			else:
+				fSup.get().rightChild.set(None)
+		# remove node with only one child
+		elif (childNum == 1):
+			if (isEmpty(sup.get().leftChild.get())):
+				childNotEmpty = sup.get().rightChild
+			else:
+				childNotEmpty = sup.get().leftChild
+			if (isEmpty(fSup.get())):
+				# the root is removed
+				currentNode.set(childNotEmpty.get())
+			elif (fSup.get().leftChild.get() == sup.get()):
+				fSup.get().leftChild.set(childNotEmpty.get())
+			else:
+				fSup.get().rightChild.set(childNotEmpty.get())
+		# remove node with two childs
+		else:
+			fSup = sup
+			# supLeftMax: node with the greatest value from the
+			# left subtree
+			supLeftMax = sup.get().leftChild
+			while (not isEmpty(supLeftMax.get().rightChild.get())):
+				fSup = supLeftMax
+				supLeftMax = supLeftMax.get().rightChild
+			sup.get().key = supLeftMax.get().key
+			if (fSup.get() == sup.get()):
+				fSup.get().leftChild.set(supLeftMax.get().leftChild.get())
+			else:
+				fSup.get().rightChild.set(supLeftMax.get().leftChild.get())
 
 
-T = BinarySearchTree()
-T.insertKey(4)
-T.insertKey(4)
-T.insertKey(2)
-T.insertKey(6)
-T.insertKey(1)
-T.insertKey(3)
-T.insertKey(5)
-T.insertKey(7)
-
-T.removeKey(5)
-preorder(T)
-T.removeKey(6)
-preorder(T)
-T.removeKey(4)
-preorder(T)
-T.removeKey(2)
-preorder(T)
-
-
-
-"""
+def removeKey(bst, key):
+	remove_i(bst.root, key)
